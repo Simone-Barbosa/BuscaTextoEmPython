dcdf818767dd16637c57d8831121bd2cbc594bce
Simone-Barbosa <simone_barbosa@icloud.com>
2022-01-28 20:40:33 +0000

    Initial commit

---
+++ exemplo_urllib3.py
@@ -0,0 +1,14 @@
+import urllib3
+
+http = urllib3.PoolManager()
+pagina = http.request('GET', 'http://www.iaexpert.com.br')
+
+statusPagina = pagina.status
+dadosPagina = pagina.data                   #Retorna todo codigo fonte da página
+dadosPaginaLimitado = pagina.data[0:50]     #Retorna os primeiros 50 caracteres do cod fonte da pagina
+
+print(statusPagina)
+#print(dadosPagina)
+print(dadosPaginaLimitado)
+
+
